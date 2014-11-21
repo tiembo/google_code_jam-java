d62de59a4be3089c67d55ec3edd4d61f3d33c8d9
Tiem Song <tiem@songfamily.org>
2014-11-20 22:27:31 -0800

    working version of reverse words (africa 2011 problem b - https://code.google.com/codejam/contest/351101/dashboard#s=p1)

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+public class Main {
+
+    private static String go(String s) {
+        String[] split = s.split("\\s");
+        int strlen = split.length;
+        String result = split[strlen - 1];
+
+        if (strlen > 1)
+            for (int i = strlen - 1; i > 0; i--)
+                result += " " + split[i - 1];
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String outfile = "/tmp/output";
+            PrintWriter writer = new PrintWriter(outfile, "utf-8");
+
+            FileReader f = new FileReader(args[0]);
+            BufferedReader reader = new BufferedReader(f);
+
+            int numCases = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < numCases; i++) {
+                writer.println("Case #" + (i + 1) + ": " + go(reader.readLine()));
+            }
+
+            writer.close();
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
